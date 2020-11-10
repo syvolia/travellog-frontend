5b26b698c904f331afe59623039ba10b9d924339
syvolia <syvoliamary@gmail.com>
2020-11-11 02:00:59 +0300

    apifilechange

--- src/API.js
+++ src/API.js
@@ -1,4 +1,4 @@
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:1337' : 'https://travel-api-log.herokuapp.com/';
+const API_URL = 'https://travel-api-log.herokuapp.com';
 
 export async function listLogEntries() {
   const response = await fetch(`${API_URL}/api/logs`);
